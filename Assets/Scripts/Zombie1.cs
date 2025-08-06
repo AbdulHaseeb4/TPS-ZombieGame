7afbecdfd26976742bddbf6e72d26dab8b5dffb6
Abdul Haseeb <abdulhaseeb0847179@gmail.com>
2025-08-06 13:50:17 +0500

    Zombie And Player Health Bar

--- Assets/Scripts/Zombie1.cs
+++ Assets/Scripts/Zombie1.cs
@@ -9,6 +9,7 @@ public class Zombie1 : MonoBehaviour
     private float zombieHealth = 100f;
     private float presentHealth;
     public float giveDamage = 5f;
+    public HealthBar healthBar;
 
     [Header("Zombie Things")]
     public NavMeshAgent zombieAgent;
@@ -40,6 +41,7 @@ public class Zombie1 : MonoBehaviour
     private void Awake()
     {
         presentHealth = zombieHealth;
+        healthBar.GiveFullHealth(zombieHealth);
         zombieAgent = GetComponent<NavMeshAgent>();
     }
 
@@ -125,8 +127,9 @@ public class Zombie1 : MonoBehaviour
     public void zombieHitDamage(float takeDamage)
     {
         presentHealth -= takeDamage;
+        healthBar.SetHealth(presentHealth);
 
-        if(presentHealth <= 0)
+        if (presentHealth <= 0)
         {
             anim.SetBool("Attacking", false);
             anim.SetBool("Walking", false);
